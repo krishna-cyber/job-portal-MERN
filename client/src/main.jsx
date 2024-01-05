272f9e0069c404c3db103014a40f06e145b2856e
krishna-cyber <tiwarikrishna54321@gmail.com>
2024-01-05 20:35:39 +0545

    react strict mode removed

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -9,9 +9,7 @@ import { store } from "./app/store";
 import { Provider } from "react-redux";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <RouterProvider router={router} />
-    </Provider>
-  </React.StrictMode>
+  <Provider store={store}>
+    <RouterProvider router={router} />
+  </Provider>
 );
